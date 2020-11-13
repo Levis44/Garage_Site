7d19c40a6465e525040b00d03229454bec70e8dc
Levi <levi.ciarrocchi@gmail.com>
2020-11-13 09:43:26 -0300

    first commit

---
+++ enviar.php
@@ -0,0 +1,29 @@
+// Destinatário
+$para = "levibc44@gmail.com";
+
+// Assunto do e-mail
+$assunto = "Contato do através do site ...";
+
+// Campos do formulário de contato
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$mensagem = $_POST['conteudo'];
+
+// Monta o corpo da mensagem com os campos
+$corpo = "<html><body>";
+$corpo .= "Nome: $nome ";
+$corpo .= "Email: $email Mensagem: $mensagem";
+$corpo .= "</body></html>";
+
+// Cabeçalho do e-mail
+$email_headers = implode("\n", array("From: $nome", "Reply-To: $email", "Subject: $assunto", "Return-Path: $email", "MIME-Version: 1.0", "X-Priority: 3", "Content-Type: text/html; charset=UTF-8"));
+
+//Verifica se os campos estão preenchidos para enviar então o email
+if (!empty($nome) && !empty($email) && !empty($mensagem)) {
+    mail($para, $corpo, $email_headers);
+    $msg = "Sua mensagem foi enviada com sucesso.";
+    echo "<script>alert('$msg');window.location.assign('https://www.lcsruas.dev/garage/index.html');</script>";
+} else {
+    $msg = "Erro ao enviar a mensagem.";
+    echo "<script>alert('$msg');window.location.assign('https://www.lcsruas.dev/garage/index.html');</script>";
+}
